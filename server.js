538241ecb2c1a0346f1feb510b9b4263423494f9
Mahmoud shorafa <m2shorafa@gmail.com>
2024-11-30 15:02:14 +0200

    make more routes

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 const dbConnection = require("./config/database");
 const ApiError = require("./utils/apiError");
 const globalError = require("./middleware/errorMiddleware");
+const { webhookCheckout } = require("./services/orderServices");
 
 dotenv.config({ path: "config.env" });
 
@@ -25,13 +28,40 @@ app.use(cors());
 app.options("*", cors());
 app.use(compression());
 
-app.use(express.json());
+// Checkout webhook
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  webhookCheckout
+);
+
+app.use(express.json({ limit: "20kb" }));
+// Middleware to protect against HTTP Parameter Pollution attacks
+app.use(
+  hpp({
+    whitelist: [
+      "price",
+      "sold",
+      "quantity",
+      "ratingsAverage",
+      "ratingsQuantity",
+    ],
+  })
+);
 app.use(express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
   console.log(`mode ${process.env.NODE_ENV}`);
 }
+
+// Limit each IP to 100 requests per `window` (here, per 15 minutes)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message:
+    "Too many accounts created from this IP, please try again after an hour",
+});
 
 //Routers
 mountRoutes(app);
